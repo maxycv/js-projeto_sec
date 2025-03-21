59822bc776bb5c5debfe8f080565fa5b53d3d35f
maxycv <maxsimao63@gmail.com>
2025-03-20 21:45:30 -0300

    primeira_logica autalizada

--- app.js
+++ app.js
@@ -4,17 +4,31 @@
 //let Paragrafo = document.querySelector ('p');
 //Paragrafo.innerHTML = 'Escolha um numero entre 1 e 100';
 
-// document.querySelector (?) Paragrafo.innerHTML altear o HTML pelo java scrip
 
 exibirTextoNatela('h1', 'jogo de adivinhaçao');
 exibirTextoNatela('p', 'escolha um numero de 1 a 100')
 
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 100) + 1;
+
+    // function dar a funçaom para o codigo
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto)
+
+
 function exibirTextoNatela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+
+    //document.querySelector (?) ??.innerHTML alterar o HTML pelo java scrip
 }
 
-function verificarChute(){alert('Apertpu o botão');
-
-
+function verificarChute(){
+let chute = parseInt (document.querySelector ('input'). value);
+console.log(chute === numeroSecreto);
 }
+     
+
+
