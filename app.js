8736d422156657de7aa558959451931e29729623
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2025-03-20 21:41:22 -0300

    update

--- app.js
+++ app.js
@@ -7,13 +7,21 @@
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10) + 1;
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 function verificarChute() {
-    console.log('Apertou o botão');
+    let chute = parseInt(document.querySelector('input').value);
+    console.log(chute === numeroSecreto);
 }
 
 //<h1>Jogo de adivinhação</h1>
