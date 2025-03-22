bfbe9bdb3e5ef27fe0e0ce8eda2239e32a7b7538
maxycv <maxsimao63@gmail.com>
2025-03-21 21:48:01 -0300

    primeira loginca atulizada 2

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
 // let subtitulo = document.querySelector('p');
 // subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
 
+
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10) + 1;
+    return parseInt(Math.random() * 100) + 1;
 }
 
 let numeroSecreto = gerarNumeroAleatorio();
@@ -18,11 +19,29 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-
+let tentativas = 3;
 function verificarChute() {
     let chute = parseInt(document.querySelector('input').value);
-    console.log(chute === numeroSecreto);
-}
+    if (tentativas === 0) {
+        
+        exibirTextoNaTela('p', 'suas tentativa acabaram! o numero era' + numeroSecreto );
+    } else {
+        tentativas--;
+        let palavratentativa = tentativas === 1 ? 'tentativa' : 'tentativas';
+        if (chute === numeroSecreto) {
+            exibirTextoNaTela('h1', `parabens voce avertou o numesecreto e ${numeroSecreto}.`)
 
+        } else if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', `erou o numero secreto e menor voce ainda tem ${tentativas}.`);
+        }
+        else { exibirTextoNaTela('p', `Errou! o nemero secreto e maior voce ainta tem ${tentativas} ${palavratentativa}`); }
+    }
+
+
+
+
+
+}
 //<h1>Jogo de adivinhação</h1>
 //<p>Escolha um número entre 1 e 100</p>
+
